f9d0ee2ff3dbd313a31d1e44ab9a739c3f6909a5
Chema19 <chemalonso96@gmail.com>
2018-07-05 20:32:36 -0500

    Initial Commit

---
+++ ExamenFinal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExamenFinal
+//
+//  Created by Josemaria Inga Villafuerte on 5/07/18.
+//  Copyright © 2018 Josemaria Inga Villafuerte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
